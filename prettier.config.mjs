dfaa1ae5447e288980fec4cb99d2c1e919e033e8
Gabriellmrs <gabrielmoriss755@gmail.com>
2025-02-25 21:10:51 -0300

    [FE-000](fix): eslint and prettier bug fixed

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,10 @@
-import * as prettierTailwindPlugin from 'prettier-plugin-tailwindcss';
+import * as prettierTailwindPlugin from 'prettier-plugin-tailwindcss'
 
 const prettierConfig = {
   singleQuote: true, // Usa aspas simples em vez de duplas
-  trailingComma: 'all',
-  plugins: [prettierTailwindPlugin],
-};
+  semi: false, // Remove os pontos e vírgulas
+  trailingComma: 'all', // Mantém as vírgulas finais sempre que possível
+  plugins: [prettierTailwindPlugin], // Organiza as classes do Tailwind CSS
+}
 
-export default prettierConfig;
+export default prettierConfig
